4bf4eaff72ea58a2b22865066946373d9e1e3491
lzz <455913979@qq.com>
2019-04-05 21:23:14 +0800

    v0.1

--- src/main/java/net/wanho/controller/LoginController.java
+++ src/main/java/net/wanho/controller/LoginController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -14,16 +15,20 @@ import javax.servlet.http.HttpSession;
 /**
  * Created by 胡梦娟 on 2018/11/27.
  */
-@Controller
+@RestController
 @RequestMapping("login")
 public class LoginController {
     @Autowired
     private StudentServiceI studentService;
 
-    @RequestMapping()
+    @RequestMapping("/")
+    String home(){
+        return "hello git!";
+    }
+ /*   @RequestMapping()
     public String showLogin(){
         return "login";
-    }
+    }*/
 
     @RequestMapping("checkLogin")
     @ResponseBody
