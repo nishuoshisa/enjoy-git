a7010f69e6b1bb8fc5df383a281f5eec1db03f46
lzz <455913979@qq.com>
2019-04-05 22:13:08 +0800

    0.6

--- src/main/java/net/wanho/controller/A.java
+++ src/main/java/net/wanho/controller/A.java
@@ -4,4 +4,7 @@ package net.wanho.controller;
  * Created by 胡梦娟 on 2019-04-05.
  */
 public class A {
+    public static void main(String[] args) {
+        System.out.println("lzz master v0.6");
+    }
 }
